1b8a7b27f244bd178c15e058f3ed9ff08e7e3ac3
Dmitry Uzyanov <arfush@cockatiel.fun>
2022-09-01 22:46:33 +0500

    feat: Add NetworkSource

--- src/main/java/nucleon/network/Network.java
+++ src/main/java/nucleon/network/Network.java
@@ -1,7 +1,35 @@
 package nucleon.network;
 
+import lombok.NonNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Network {
+    private final Map<Class<? extends NetworkSource>, NetworkSource> sources = new HashMap<>();
+
     private boolean started = false;
+
+    public void register(@NonNull NetworkSource source) {
+        if (sources.containsKey(source.getClass())) {
+            throw new IllegalArgumentException(String.format("Source `%s` already registered", source.getClass()));
+        }
+        // TODO: Call cancellable event
+        sources.put(source.getClass(), source);
+        if (started) {
+            source.startup();
+        }
+    }
+
+    public void unregister(Class<? extends NetworkSource> sourceClass) {
+        if (!sources.containsKey(sourceClass)) {
+            throw new IllegalArgumentException(String.format("Source `%s` not registered", sourceClass));
+        }
+        if (started) {
+            sources.get(sourceClass).shutdown();
+        }
+        sources.remove(sourceClass);
+    }
 
     public void startup() {
         if (started) {
---
+++ src/main/java/nucleon/network/NetworkSource.java
@@ -0,0 +1,7 @@
+package nucleon.network;
+
+public abstract class NetworkSource {
+    public abstract void startup();
+
+    public abstract void shutdown();
+}
