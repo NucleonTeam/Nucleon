eac2f1d0033093210014f599cf9e4b15d51ccdfc
Dmitry Uzyanov <arfush@cockatiel.fun>
2022-09-01 23:41:39 +0500

    feat: Add Server

---
+++ src/main/java/nucleon/server/Server.java
@@ -0,0 +1,32 @@
+package nucleon.server;
+
+public class Server {
+    private static Server instance = null;
+
+    public static Server getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("Server instance not created");
+        }
+        return instance;
+    }
+
+    private boolean started = false;
+
+    public Server() {
+        instance = this;
+    }
+
+    public void startup() {
+        if (started) {
+            throw new IllegalStateException("Server instance already started");
+        }
+        started = true;
+    }
+
+    public void shutdown() {
+        if (!started) {
+            throw new IllegalStateException("Server instance not started");
+        }
+        started = false;
+    }
+}
