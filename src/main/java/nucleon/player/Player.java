44bda595a20c8df0658ded43d1cab20f113b3f69
qPexLegendary <terentev.andrey.2002@gmail.com>
2022-12-12 21:41:07 +0700

    feat(player): added PlayerManager

--- src/main/java/nucleon/player/Player.java
+++ src/main/java/nucleon/player/Player.java
@@ -1,18 +1,54 @@
 package nucleon.player;
 
+import com.nukkitx.network.util.DisconnectReason;
 import com.nukkitx.protocol.bedrock.BedrockServerSession;
 import lombok.Getter;
+import lombok.NonNull;
 
 import java.util.UUID;
 
 public final class Player {
 
-    @Getter private final UUID uuid = UUID.randomUUID();
+    @Getter private final UUID id = UUID.randomUUID();
     @Getter private final PlayerChainData chainData;
     private final BedrockServerSession session;
+
+    @Getter private boolean disconnected = false;
 
     public Player(PlayerChainData chainData, BedrockServerSession session) {
         this.chainData = chainData;
         this.session = session;
+
+        onLogin();
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+        if (!(obj instanceof Player)) return false;
+
+        return id.equals(((Player) obj).id);
+    }
+
+    public @NonNull String getName() {
+        return chainData.getUsername();
+    }
+
+    private void onLogin() {
+        PlayerManager.getInstance().addPlayer(this);
+        session.addDisconnectHandler(this::onQuit);
+        System.out.println("joined");
+    }
+
+    private void onQuit(DisconnectReason reason) {
+        disconnected = true;
+        PlayerManager.getInstance().removePlayer(this);
+        System.out.println("quited");
     }
 }
